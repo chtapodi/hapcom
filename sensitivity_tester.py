a6e2fa452f1c5bf2d0c3482263c161b1c10d8481
chtapodi <x.t.quinn@gmail.com>
2019-12-01 07:56:14 +0000

    seems to be working, though edge cases have yet to be tested

--- sensitivity_tester.py
+++ sensitivity_tester.py
@@ -2,8 +2,13 @@
 
 import RPi.GPIO as GPIO
 import readchar
+import termios
 import time
 import random
+import pickle
+import sys
+import tty
+import select
 
 def init_gpio(pin, freq) :
 	GPIO.setwarnings(False)
@@ -13,30 +18,52 @@ def init_gpio(pin, freq) :
 	p.start(50) #50% duty, makes freq=freq
 	return p
 
+def isData(): #https://stackoverflow.com/a/2409034
+	return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
 
 def test_freq(pwm, dict, base_freq, compare_freq) :
-	key=null
-	while key not in ["q", "s", "d"] :
-		key=readchar.readkey()
-		pwm.ChangeFrequency(base_freq)
-		time.sleep(1)
-		pwm.ChangeFrequency(compare_freq)
-		time.sleep(1)
+	key=None
+
+	old_settings = termios.tcgetattr(sys.stdin)
+	try:
+		tty.setcbreak(sys.stdin.fileno())
+
+		while True:
+			pwm.ChangeFrequency(base_freq)
+			print(base_freq)
+			time.sleep(1)
+			pwm.ChangeFrequency(compare_freq)
+			print(compare_freq)
+			time.sleep(1)
+
+			if isData():
+				key = sys.stdin.read(1)
+				if key in ["q", "s", "d"]:
+					break
+
+	finally:
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
 
 	if key=='s' : #detected as the same
 		insert_freq(dict, base_freq, compare_freq, False)
+		print("same")
 	elif key=='d' :
-		insert_freq(dict, base_freq, compare_freq, False)
+		insert_freq(dict, base_freq, compare_freq, True)
+		print("different")
+
 	else :
 		print("you broke the loop.")
 
-
+#inserts freq data
 def insert_freq(dict, base_freq, compare_freq, detection_status) :
 	if base_freq not in dict :
 		dict[base_freq]={True:[], False:[]}
 	dict[base_freq][detection_status].append(abs(base_freq-compare_freq))
+	print(dict)
 
-
+#selects a base_freq
 def get_base_feq(dict, min, max, max_diff) :
 	val=random.randrange(min,max)
 	for key in dict.keys():
@@ -44,32 +71,45 @@ def get_base_feq(dict, min, max, max_diff) :
 			val=key
 	return val
 
-def get_compare_freq(dict, base_freq, max_diff) :
 
+#generates a frequency from the base_freq
+def get_compare_freq(dict, base_freq, max_diff) :
+	compare_freq=None
 	if base_freq in dict : #if this freq has already been used
 		success=dict[base_freq][True]
 		failiure=dict[base_freq][False]
-		if abs(max(failiure)-min(success))<=1 :
-			return null #the distinction has been found
+		if (len(success)>0 and len(failiure)>0) and (abs(max(failiure)-min(success))<=1) :
+			compare_freq=None #the distinction has been found
 		else :
 			val=random.randrange(base_freq-max_diff,base_freq+max_diff)
 			while (abs(val-base_freq) not in success and abs(val-base_freq) not in failiure) : #This will randomly choose values within the given range unless the value has previously been used.
 				val=random.randrange(base_freq-max_diff,base_freq+max_diff)
-			return val
+			compare_freq=val
+	else :
+		compare_freq=random.randrange(base_freq-max_diff,base_freq+max_diff)
+
+	return compare_freq
 
 
 def run_tests(pwm, min_freq, max_freq, max_diff) :
-	null_counter=0
+	None_counter=0
 	freq_log={}
+	print("starting tests")
+	num_vals=int((max_freq-min_freq)/max_diff)
+	print("num_vals ", num_vals)
 
 	try :
-		while null_counter<int((max_freq-min_freq)/max_diff) :
+		while None_counter<num_vals :
 			base_freq=get_base_feq(freq_log, min_freq, max_freq, max_diff)
 
 			compare_freq=get_compare_freq(freq_log, base_freq, max_diff)
-
-			test_freq(pwm, freq_log, base_freq, compare_freq)
+			if compare_freq==None :
+				None_counter+=1
+				print("already completed")
+			else :
+				test_freq(pwm, freq_log, base_freq, compare_freq)
 	except KeyboardInterrupt :
+		print("broke")
 		pickle.dump( freq_log, open( "freq_log.p", "wb" ) )
 
 def main():
@@ -80,9 +120,8 @@ def main():
 	max_diff=20
 
 	pwm=init_gpio(pin, freq)
-
+	print("initialized")
 	run_tests(pwm, min_freq, max_freq, max_diff)
 
 if __name__== "__main__":
   main()
-
